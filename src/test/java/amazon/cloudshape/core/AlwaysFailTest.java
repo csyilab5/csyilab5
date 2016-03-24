1133b01467fc955165966174b3eba0d1ec4d70b0
EC2 Default User <ec2-user@ip-172-31-37-174.eu-west-1.compute.internal>
2016-03-24 07:44:38 +0000

    revertTest

--- src/test/java/amazon/cloudshape/core/AlwaysFailTest.java
+++ src/test/java/amazon/cloudshape/core/AlwaysFailTest.java
@@ -7,8 +7,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 	public class AlwaysFailTest {
+	
+	@Test
 	public void fail() {
-		throw new BlobNotFoundException();
+	     
+		//	throw new BlobNotFoundException();
 	{
 }
 

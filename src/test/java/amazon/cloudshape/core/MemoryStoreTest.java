6610c4d469b282e0e8baee4c2e453745af768e3a
EC2 Default User <ec2-user@ip-172-31-43-236.eu-west-1.compute.internal>
2016-03-25 07:22:56 +0000

    add test

--- src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
+++ src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
@@ -69,4 +69,8 @@ public class MemoryStoreTest {
         store.deleteBlob(BUCKET_NAME, key);
         store.loadBlob(BUCKET_NAME, key);
     }
+    @Test
+    public void badTest(){
+	throw new BlobNotFoundException();
+    }
 }

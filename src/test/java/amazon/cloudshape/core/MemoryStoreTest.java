a9da3b09aa5c35124ec5ab57dbf3f674597bbfed
EC2 Default User <ec2-user@ip-172-31-47-139.eu-west-1.compute.internal>
2016-03-25 07:12:50 +0000

    Revert "Added failed test"
    This reverts commit 989dc69a7b980dfdda72e21685d98d4404cb475d.

--- src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
+++ src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
@@ -69,7 +69,4 @@ public class MemoryStoreTest {
         store.deleteBlob(BUCKET_NAME, key);
         store.loadBlob(BUCKET_NAME, key);
     }
-@Test
-public void testFails(){
-throw new BlobNotFoundException();}
 }

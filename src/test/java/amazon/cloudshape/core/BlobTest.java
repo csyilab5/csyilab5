870b07f1c69c59cd549b3da2cd855ecfd9cc2883
qwiklab-dorian <nan80041@figjs.com>
2016-03-25 08:58:54 +0200

    Create BlobTest.java
    Added new test

---
+++ src/test/java/amazon/cloudshape/core/BlobTest.java
@@ -0,0 +1,16 @@
+package amazon.cloudshape.core;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BlobTest {
+
+    @Test
+    public void blob() {
+        throw new BlobNotFoundException();
+    }
+}

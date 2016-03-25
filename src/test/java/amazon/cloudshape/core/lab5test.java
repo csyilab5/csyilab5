b3c4e4c147a4cb9e53537bad5bb177bf34ad8271
alinionel91 <alinionelPO@gmail.com>
2016-03-25 09:49:47 +0200

    Create lab5test.java

---
+++ src/test/java/amazon/cloudshape/core/lab5test.java
@@ -0,0 +1,17 @@
+package amazon.cloudshape.core;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class lab5test {
+
+
+    @Test()
+    public void deleteBlobs() {
+        throw new BlobNotFoundException();
+    }
+}

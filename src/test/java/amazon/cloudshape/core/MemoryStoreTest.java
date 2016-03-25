f755e7a9cc897fbdb93333dd3b499eba8053ed9a
EC2 Default User <ec2-user@ip-172-31-45-205.eu-west-1.compute.internal>
2016-03-25 07:13:41 +0000

    Revert "test commit"
    This reverts commit d988767b0aef815aad8e6b80f777df4cbca2c237.

--- src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
+++ src/test/java/amazon/cloudshape/core/MemoryStoreTest.java
@@ -12,11 +12,6 @@ public class MemoryStoreTest {
     private static final String BUCKET_NAME = "iphone";
 
     @Test
-public void test(){
-	throw new BlobNotFoundException();
-}	
-    
-    @Test
     public void createBuckets() {
         DataStore store = new MemoryStore();
 

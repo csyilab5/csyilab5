01587abaa4e0652008faa090b33334a6e28e28a2
Spac Valentin <spac.valentin@gmail.com>
2016-03-25 09:06:12 +0200

    added failing test

---
+++ src/test/java/amazon/cloudshape/core/BlobTest .java
@@ -0,0 +1,17 @@
+package amazon.cloudshape.core;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class BlobTest {
+
+    @Test
+    public void createBuckets() {
+       throw new BlobNotFoundException();
+    }
+
+}
